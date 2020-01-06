6b47c48e425e8ebfa170787135543f965b115b5c
Chris Phillips <xposure@users.noreply.github.com>
2020-01-06 17:52:40 -0500

    nothing to see here

--- AggCommon/Assembly.cs
+++ AggCommon/Assembly.cs
@@ -1,1 +1,1 @@
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("AggTest")]
+//[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("AggTest")]
--- AggCommon/IAggregateEvents.cs
+++ AggCommon/IAggregateEvents.cs
@@ -7,4 +7,14 @@ namespace AggCommon
     {
         Task<IAggregrateEvent<T>> Append<TEvent>(IAggregrate<T> aggregrate, TEvent eventData) where TEvent : IEventState<T>;
     }
+
+    // public interface IAggregrateEvents
+    // //where T : IAggregrate<T>
+    // {
+    //     Task<IAggregrateEvent<TAggregate>> Append<TAggregate, TEvent>(IAggregrate aggregrate, TEvent eventData) 
+    //         where TAggregate: IAggregrate<TAggregate>
+    //         where TEvent : IEventState<TAggregate>;
+    //     //Task<IAggregrateEvent<T>> Read(int start = 0, int length = -1);
+    //     //Task Delete(IAggregrate<T> aggregrate);
+    // }
 }
